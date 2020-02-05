77dd30ac735ab90ba6cb13f94dba61567c5e5613
Tim Herrmann <tvherrmann@comcast.net>
2020-02-04 18:10:21 -0800

    Modified Test File

--- grammar/__tests__/grammar_errors.test.js
+++ grammar/__tests__/grammar_errors.test.js
@@ -7,15 +7,17 @@
 
 const syntaxCheck = require('../syntax-checker');
 
+//Examples from Toal's
+//['keyword as id', 'else := 5'],
+//['unclosed paren', 'let var x := (2 * 3 in end'],
+//['unknown operator', 'x := 2 ** 5'],
+//['chained relational operators', '1 < 3 < 5'],
+//['unclosed comment', 'x := /*   hello 9'],
+//['bad unicode escape', '"ab\\u{1f4%a9}c"'],
+//['bad escape', '"ab\\q"'],
+//['bad character in id', '$x := 1'],
 const errors = [
-  //['keyword as id', 'else := 5'],
-  //['unclosed paren', 'let var x := (2 * 3 in end'],
-  //['unknown operator', 'x := 2 ** 5'],
-  //['chained relational operators', '1 < 3 < 5'],
-  //['unclosed comment', 'x := /*   hello 9'],
-  //['bad unicode escape', '"ab\\u{1f4%a9}c"'],
-  //['bad escape', '"ab\\q"'],
-  //['bad character in id', '$x := 1'],
+
     ['ambiguous statements', 'x = 3x = 3'],
 ];
 
