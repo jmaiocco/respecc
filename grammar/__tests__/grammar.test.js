6138bbe021cc067076677e6e558ce6778fe2bd51
Luis <luisfergar99@gmail.com>
2020-02-08 00:53:44 -0800

    adds more sample programs

--- grammar/__tests__/grammar.test.js
+++ grammar/__tests__/grammar.test.js
@@ -6,159 +6,253 @@
  */
 
 const syntaxCheck = require("../syntax-checker");
-/*
-const program1 = String.raw`
-let
-  type Circle = {
-    x: int,
-    y: int,
-    color: string
-  }
-  function fib(n: int): int =
-    let
-      var a: int := 0
-      var b: int := 1
-      var t := 0
-    in
-      while n > 0 do
-        (t := a; a := b; b := t + b; n := n - 1);
-      "abc$%π\n\\\u{41}";
-      a := if 1 then b := 1;
-      b := if 0 then 2 else let var x := 1 in x end;
-      b
-    end
-  var c: Circle := Circle {y = 2, x = 5<3&2<>1, color = "blue"}
-  type list = array of string
-  var dogs: list := list [3] of "woof"
-in
-  dogs[1] := "Sparky";
-  for i := 1 to 10 do
-    print(fib(i) & 0 | 1 + 0 * 1 - 0 / 1)
-end
-`;
-*/
-let programs = [
-  String.raw`
-Salutations!
-for( gimme i : Number = 0; i < 10; i++) could you...
-  print(i)
-Thank You.
-Farewell!
-`
-];
-programs.push(String.raw`
-Salutations!
-for( gimme i : Number = 0; i < 10; i++) could you...
-  print(i)
-Thank You.
-Farewell!
-`);
 
-programs.push(String.raw`
-Excuse me, if x is less than 3, could you...
-	x = 3;
-  Please declare x as 3.
-  Please declare x as x < 4.
-Thank You.
-Otherwise, if x is equal to 6, could you...
-Thank You.
-Otherwise, could you...
-Thank You.
-`);
-
-programs.push(String.raw`
+const politeChangemaker = String.raw`
 Hello!
-if(x raised to the power of 3 is less than 6) {
-	x = 3;
-}
 
+Please declare US_Denominations as a Number as [25, 10, 5, 1].
+
+Favor MakeChange(amount) could you...
+  Excuse me, if (amount is less than 0), could you...
+    Do me a favor and run print with ("Error").
+    Kindly return -1
+  Thank You.
+  Please declare result as a Number[].
+  Please populate remaining with amount.
+  Please populate i with 0.
+  Excuse me, while i is less than 4, could you...
+    Please populate result[ i ] with remaining / US_Denominations[ i ].
+    Please populate remaining with remaining modded with US_Denominations[ i ].
+    i++
+  Thank You.
+  Kindly return result
+Thank You.
+
+Please populate exampleAmount with 105.
+Do me a favor and run MakeChange with (exampleAmount).
+
+Bye Bye!
+  `;
+
+const politeFibonacci = String.raw`
+Salutations!
+
+Favor Fibonacci(n: Number, length: Number) could you...
+  Excuse me, if (n is equal to 1), could you...
+    Please populate series with [0, 1].
+    Kindly return series
+  Thank You.
+  Otherwise, could you...
+    Please populate series with Fibonacci(n minus 1, length minus 1).
+    Please populate series[length] with (series[length minus 1] plus series[length minus 2]).
+    Kindly return series
+  Thank You.
+Thank You.
+
+Do me a favor and run Fibonacci with (12, 12).
 Farewell!
-`);
+`;
 
-programs.push(String.raw`
-y = func(x,y,z) or func();
-Please populate y with the result of running sum with (a,b).
+const politeGCD = String.raw`
+Hey!
 
-x =  the result of running sum with (a,b)
-
-You deserve a break!
-`);
-programs.push(String.raw`
-gimme id : Number = 6
-Please declare id as a Number as 6.
-id = 7;
-Please populate id with 7.
-
-`);
-
-programs.push(String.raw`
-Excuse me, while x is less than y, could you...
-	 x++
-
+Favor GCD(firstValue: Number, secondValue: Number) could you...
+  Excuse me, if (firstValue is less than 0), could you...
+    Please populate firstValue with (0 - firstValue).
+  Thank You.
+  Excuse me, if (secondValue is less than 0), could you...
+    Please populate secondValue with (0 - secondValue).
+  Thank You.
+  Excuse me, while (secondValue is greater than 0), could you...
+    Please populate temporaryValue with secondValue.
+    Please populate secondValue with firstValue modded with secondValue.
+    Please populate firstValue with temporaryValue.
+  Thank You.
+  Kindly return firstValue
 Thank You.
 
-for(gimme i =  x; x < 6; x++) {
+Do me a favor and run GCD with (90, 180).
+
+Bye Bye!
+`;
+
+const politeIsPrime = String.raw`
+Hi There!
+Favor isPrime(num) could you...
+ Excuse me, if (num is less than or equal to 1), could you...
+    Kindly return Yes
+  Thank You.
+  Otherwise, if (num is less than or equal to 3), could you...
+    Kindly return Yes
+  Thank You.
+  Otherwise, if (num%2 is equal to 0  or (num%3 is equal to 0)), could you...
+    Kindly return No
+  Thank You.
+
+  Please populate i with 5.
+  Excuse me, while i*i is less than or equal to 0 or ( num%(i+2) is equal to 0 ), could you...
+    Kindly return Yes
+  Thank You.
+  i = i + 6
+  Kindly return No
+Thank You.
+Farewell!
+`;
+
+const politePseudoRandom = String.raw`
+Hey!
+//Generate pseudorandom number
+Please populate a with 1103515245.
+  Please populate m with 2 raised to the power of 31.
+  Please populate c with 12345.
+  Please populate x with 4.
+
+Favor linearCongruentialGenerator() could you...
+  Excuse me, if x is less than or equal to 0 and x is less than m, could you...
+    Please populate x with (a times x plus c) modded with m.
+    Kindly return x
+  Thank You.
+  Kindly return "invalid input"
+Thank You.
+
+Bye Bye!
+`;
+describe("The (polite) syntax checker", () => {
+  test("accepts sample changemaker in polite form", done => {
+    expect(syntaxCheck(politeChangemaker)).toBe(true);
+    done();
+  });
+
+  test("accepts sample fibonnacci function in polite form", done => {
+    expect(syntaxCheck(politeFibonacci)).toBe(true);
+    done();
+  });
+
+  test("accepts sample GCD program in polite form", done => {
+    expect(syntaxCheck(politeGCD)).toBe(true);
+    done();
+  });
+  test("accepts sample prime checker in polite form", done => {
+    expect(syntaxCheck(politeIsPrime)).toBe(true);
+    done();
+  });
+  test("accepts sample PseudoRandom number generator in polite form", done => {
+    expect(syntaxCheck(politePseudoRandom)).toBe(true);
+    done();
+  });
+});
+
+//Rude Forms of Programs
+const rudeChangemaker = String.raw`
+gimme US_Denominations = [25, 10, 5, 1]
+
+function MakeChange(amount) {
+  if (amount < 0) {
+    print("Error")
+    return -1
+  }
+  result = []
+  remaining = amount
+  i = 0
+  while ( i < 4 ) {
+    result[ i ] = remaining / US_Denominations[ i ]
+    remaining= remaining % US_Denominations[ i ]
+    i++
+  }
+  return result
+}
+exampleAmount = 105
+MakeChange(exampleAmount)
+`;
+
+const rudeFibonacci = String.raw`
+function Fibonacci(n, length) {
+  if (n == 1) {
+    series = [0, 1]
+    return series
+  } else {
+    series = Fibonacci(n - 1, length - 1)
+    series[length] = series[length minus 1] plus series[length minus 2]
+    return series
+  }
 }
 
-if(  x  < y   ) {
+Fibonacci(12, 12)
+`;
+
+const rudeGCD = String.raw`
+function GCD(firstValue, secondValue) {
+  if (firstValue < 0) {
+    firstValue = (0 - firstValue)
+  }
+  if (secondValue < 0) {
+    secondValue = (0 - secondValue)
+  }
+  while (secondValue > 0) {
+    temporaryValue = secondValue
+    secondValue = firstValue % secondValue
+    firstValue = temporaryValue
+  }
+  return firstValue
 }
 
-x = the result of running o
-`);
+GCD(90, 180)
+`;
 
-programs.push(String.raw`
-Favor o():Number {
+const rudeIsPrime = String.raw`
+function isPrime (num) {
+  if (num <= 1) {
+    return Yes
+  } else if (num <= 3) {
+    return Yes
+  } else if (num%2 == 0 || num%3 == 0) {
+    return No
+  }
+
+  i = 5
+  while (i*i <= num) {
+    if (num%i == 0 || num%(i+2) == 0) {
+      return false
+    }
+    i = i+6
+  }
+  return Yes
 }
-print(333333)
-Do me a favor and run o.
-`);
+`;
 
-programs.push(String.raw`
-/* Impolite Function Declartion */
-
-function sum(a,b) {
-	return a + b
-}`);
-
-programs.push(String.raw`
-/* Polite Function Declartion */
-Favor sum( a , b ) could you...
-	return a plus b
-Thank You.
-`);
-
-programs.push(String.raw`
-/* Impolite Loop  */
-for(gimme i = 0; i < n; i++) {
-	if(i == 3) { break; }
- }
- `);
-
-programs.push(String.raw`
-/* Polite Loop */
-Please declare i as 0.
-Excuse me, while i is less than n, could you...
-	Excuse me, if i is equal to 3, could you...
-    	break
-    Thank You.
-    Please populate i with i plus 1.
-Thank You.
-
-`);
-
-programs.push(String.raw`
-x = x ? x : x
-x = (x,x) -> could you...
-x = x + x
-Thank You.
-
-`);
-
-programs.forEach(program => {
-  describe("The syntax checker", () => {
-    test("accepts the mega program with all syntactic forms", done => {
-      expect(syntaxCheck(program)).toBe(true);
-      done();
-    });
+const rudePseudoRandom = String.raw`
+//Generates pseudorandom number
+a = 1103515245
+m = 2**31
+c = 12345
+x = 4
+function linearCongruentialGenerator(){
+  if (x>=0 && x< m){
+    x= (a* X + c) % m;
+    return  x
+  }
+  return "invalid input"
+}
+`;
+describe("The (rude) syntax checker", () => {
+  test("accepts sample changemaker in rude form", done => {
+    expect(syntaxCheck(rudeChangemaker)).toBe(true);
+    done();
+  });
+  test("accepts sample Fibonacci function in rude form", done => {
+    expect(syntaxCheck(rudeFibonacci)).toBe(true);
+    done();
+  });
+  test("accepts sample GCD function in rude form", done => {
+    expect(syntaxCheck(rudeGCD)).toBe(true);
+    done();
+  });
+  test("accepts sample prime checker in rude form", done => {
+    expect(syntaxCheck(rudeIsPrime)).toBe(true);
+    done();
+  });
+  test("accepts sample pseudorandom number generator in rude form", done => {
+    expect(syntaxCheck(rudePseudoRandom)).toBe(true);
+    done();
   });
 });
