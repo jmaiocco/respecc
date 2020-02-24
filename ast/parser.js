9b64dc7b6ae8d967d8a5e57efae6240af93c3a8a
Tim Herrmann <tvherrmann@comcast.net>
2020-02-23 22:34:57 -0800

    Implemented more rigid Inline Blocks

--- ast/parser.js
+++ ast/parser.js
@@ -194,10 +194,16 @@ const astGenerator = grammar.createSemantics().addOperation("ast", {
   Args(_open, exps, _close) {
     return exps.ast();
   },
-  Block_polite(_open, _1, sfirst, _2, ss, _3, _close, _4) {
+  Block_polite(_open, _1, sfirst, _2, ss, _3, _close) {
     return new Block([sfirst.ast(), ...ss.ast()], true);
   },
   Block_impolite(_open, _1, sfirst, _2, ss, _3, _close) {
+    return new Block([sfirst.ast(), ...ss.ast()], false);
+  },
+  InlineBlock_polite(_open, _1, sfirst, _2, ss, _3, _close) {
+    return new Block([sfirst.ast(), ...ss.ast()], true);
+  },
+  InlineBlock_impolite(_open, _1, sfirst, _2, ss, _3, _close) {
     return new Block([sfirst.ast(), ...ss.ast()], false);
   },
   Exp_ternary(exp1, _1, exp2, _2, exp3) {
