a229de0b87f66dac99aee5e1c1996dd92602c06e
Joe <nintenjoe98@gmail.com>
2020-04-23 12:02:22 -0700

    Angelic code example causing penalties to occur for some reason

--- backend/javascript-generator.js
+++ backend/javascript-generator.js
@@ -128,15 +128,15 @@ class Penalty {
   }
 }
 
-const NumbersAreStrings = new Penalty([0.5, 0.25, 0.1, -1, -1], obj => {
+const NumbersAreStrings = new Penalty([0.5, 0.25, 0.1, 0, 0], obj => {
   return `"${obj.value}"`;
 });
 
-const NumbersAreAdjusted = new Penalty([0.5, 0.25, 0.1, -1, -1], obj => {
+const NumbersAreAdjusted = new Penalty([0.5, 0.25, 0.1, 0, 0], obj => {
   return `${obj.value + Math.floor(Math.random() * 10)}`;
 });
 
-const StringsAreReversed = new Penalty([0.5, 0.25, 0.1, -1, -1], obj => {
+const StringsAreReversed = new Penalty([0.5, 0.25, 0.1, 0, 0], obj => {
   return `"${obj.value
     .split("")
     .reverse()
